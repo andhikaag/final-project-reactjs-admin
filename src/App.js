6a5857472be30260a83036fe15d1ec4e6deed63f
andhikaag <41301742+andhikaag@users.noreply.github.com>
2020-01-16 15:54:14 +0700

    menambahkan tampilan login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from '../src/components/pages/login'
 
 function App() {
   return (
     <div>
+      <Login />
     </div>
   );
 }
--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
+import { Form, Button, Container } from 'react-bootstrap'
+import AdminLogo from '../../images/admin.png'
 
-export default class index extends Component {
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
     }
+
+    this.onChangeUsername = this.onChangeUsername.bind(this)
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({ username: e.target.value })
+  }
+  onChangePassword = (e) => {
+    this.setState({ password: e.target.value })
+  }
+
+  render() {
+    return (
+      <Container>
+        <div className="d-flex justify-content-center h-100">
+          <div className="user_card">
+            <div className="d-flex justify-content-center">
+              <Form>
+                <center>
+                  <img className="imageAdmin" src={AdminLogo} width="100" height="100" />
+                </center>
+                <div className="input-group mb-3">
+                  <div className="input-group-append">
+                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                  </div>
+                  <Form.Control type="text" placeholder="Username" value={this.state.username} onChange={this.onChangeUsername} />
+                </div>
+                <div className="input-group mb-3">
+                  <div className="input-group-append">
+                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                  </div>
+                  <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={this.onChangePassword} />
+                </div>
+                <div className="d-flex justify-content-center mt-3 login_container">
+                  <Button className="btn login_btn" type="submit">
+                    Login
+                    </Button>
+                </div>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </Container>
+    )
+  }
 }
